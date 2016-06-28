f3f30bff676017bde170ddf3811e88b265b06891
Ed Mechem <email": null>
2016-06-28 16:53:35 -0700

    Cleanup code, switch Gemfile to use rubygems URL

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -4,15 +4,10 @@ get '/sessions/new' do
 end
 
 post '/sessions/new' do
-  #find user based on username
   @user = User.find_by(username: params[:username])
   password = params[:password]
 
-  #validate user based on valid password (using bcrypt)
-  #if it does
-  #&& is checking if user is not nill !!
   if @user && @user.authenticate(password)
-    #set the user-id to session
     session[:user_id] = @user.id
     redirect '/'
   else
